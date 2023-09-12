a9bb181d98eb69dda9fa3d9783d8902fe4f0a4c9
Juanchobanano <juancepeda.gestion@gmail.com>
2023-09-12 11:16:00 +0300

    Modifications to Yahoo, Brave and Bing

--- extra/test.py
+++ extra/test.py
@@ -1,4 +1,4 @@
-from search_engines_kit import Google, Bing
+from search_engines_kit import Google, Bing, Yahoo, Brave
 import requests
 from requests.auth import HTTPProxyAuth
 
@@ -6,9 +6,10 @@ proxy = "brd.superproxy.io:22225"
 username = "brd-customer-hl_93b5fa1a-zone-data_center"
 password = "ask3gh8zga95"
 
-engine = Google(proxy = proxy, username = username, password = password, language = "ES")
+engine = Yahoo(proxy = proxy, username = username, password = password, language = "ES")
 
 results = engine.search("MSI GT77 Titan 12UGS-008 17.3 / Nvidia GeForce RTX 3070 Ti UHD Intel Core i9-12900HX 1.5GHz / 128GB RAM / 2TB SSD", pages = 1)
+#results = engine.search("Hello world", pages = 2)
 #print(results.titles())
 
 for result in results.titles():
